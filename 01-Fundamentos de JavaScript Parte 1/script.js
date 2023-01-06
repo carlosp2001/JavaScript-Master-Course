7d1318632322ab25f28fcab833d6d773455b2ab5
Carlos Pineda <cara_p@unicah.edu>
2023-01-05 22:31:52 -0600

    Valores y variables

---
+++ 01-Fundamentos de JavaScript Parte 1/script.js
@@ -0,0 +1,41 @@
+let js = "amazing";
+// if (js === "amazing") alert("JavaScript es divertido");
+
+40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10);
+
+// Valores y variables
+// Valor es un pedazo de dato es la unidad de información más fundamental que tenemos en programacion / JavaScript
+
+// Valores
+console.log('Jonas');
+console.log('23');
+
+// Variables almacenamos los valores para poderlos utilizar una y otra vez
+
+let firstName = "Jonas";
+
+// Usamos la variable
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Reglas para nombrar variables
+//
+// - Principalmente usar camelCase
+// - No usar letras en el principio del nombre de la variable
+// - El nombre solo puede contener numeros, letras, guiones bajos o el signo de dolar
+
+let jonas_matilda = "JM";
+
+// Las constantes estan nombradas en mayusculas
+
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'Programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
