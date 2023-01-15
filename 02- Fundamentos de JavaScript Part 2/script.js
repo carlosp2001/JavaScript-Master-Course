4882a6bd685d64b6591193075184e5768111c91f
Carlos Pineda <cara_p@unicah.edu>
2023-01-14 21:16:44 -0600

    Modo estricto / Strict Mode

---
+++ 02- Fundamentos de JavaScript Part 2/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+// if (passTest) hasDriverLicense = true; // Probar el strict mode
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Audio'; // Palabra interface reservada
