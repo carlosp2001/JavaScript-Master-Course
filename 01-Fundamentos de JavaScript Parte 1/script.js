5c0f745a1b0056bc7b47aef4e2a17b4c7b2bc1f7
Carlos Pineda <cara_p@unicah.edu>
2023-01-14 20:38:51 -0600

    Operador condicional o ternario

--- 01-Fundamentos de JavaScript Parte 1/script.js
+++ 01-Fundamentos de JavaScript Parte 1/script.js
@@ -464,3 +464,24 @@ if (23 > 10) {
 
 const me = 'Jonas';
 console.log(`I'm ${2037 - 1991} years old ${me}.`);
+
+///////////////////////////////////////////
+// Operador condicional / operador ternario
+
+const age = 23;
+age >= 18 ? console.log('I like to drink wine') : console.log('I like to drink water');
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+	drink2 = 'wine';
+} else {
+	drink2 = 'water';
+}
+
+console.log(drink2);
+
+// Ejemplo de operador ternario dentro de una plantilla literal
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
