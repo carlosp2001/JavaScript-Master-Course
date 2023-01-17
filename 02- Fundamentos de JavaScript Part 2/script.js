11f200169f0bd07590745d229c660cc2075bfd8e
Carlos Pineda <cara_p@unicah.edu>
2023-01-17 09:19:47 -0600

    Funciones

--- 02- Fundamentos de JavaScript Part 2/script.js
+++ 02- Fundamentos de JavaScript Part 2/script.js
@@ -8,3 +8,35 @@ if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Audio'; // Palabra interface reservada
+
+///////////////////////////////////
+// Funciones
+
+// Una funcion es simplemente un fragmento de codigo que podemos reutilizar una y otra vez
+
+function logger() {
+	console.log('My name is Jonas');
+}
+
+// Calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+	console.log(apples, oranges);
+	const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+	return juice;
+
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+// console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+// Built-in funcion ejemplo
+
+const num = Number('23');
