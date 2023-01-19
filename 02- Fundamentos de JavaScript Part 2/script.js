5a56cd58af4d2ee9f3fb1bcc0608479fb40e06f6
Carlos Pineda <cara_p@unicah.edu>
2023-01-18 20:59:16 -0600

    Coding challenge #2

--- 02- Fundamentos de JavaScript Part 2/script.js
+++ 02- Fundamentos de JavaScript Part 2/script.js
@@ -234,6 +234,33 @@ if (friends1.includes('Steven')) {
 	console.log('You have a friend called Steven');
 }
 
+/////////////////////////////////////////////
+// Coding Challenge #2
+
+/*Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+Your tasks:
+	1. Writeafunction'calcTip'thattakesanybillvalueasaninputandreturns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100
+2. Andnowlet'susearrays!Socreateanarray'bills'containingthetestdata below
+3. Createanarray'tips'containingthetipvalueforeachbill,calculatedfrom the function you created before
+4. Bonus:Createanarray'total'containingthetotalvalues,sothebill+tip Test data: 125, 555 and 44
+Hint: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+Your tasks:
+	1. Writeafunction'calcTip'thattakesanybillvalueasaninputandreturns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100
+2. Andnowlet'susearrays!Socreateanarray'bills'containingthetestdata below
+3. Createanarray'tips'containingthetipvalueforeachbill,calculatedfrom the function you created before
+4. Bonus:Createanarray'total'containingthetotalvalues,sothebill+tip Test data: 125, 555 and 44
+Hint: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉*/
+
+// const calcTipA = function (bill) {
+// 	return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// }
+
+const calcTipA = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [125, 555, 44];
+const tips = [calcTipA(bills[0]), calcTipA(bills[1]), calcTipA(bills[2])];
+const totals = [bills[0] + tips[1], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips, totals);
 
 
 
