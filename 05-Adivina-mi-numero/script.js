dad41ee72bd2e38789ccd2234b4d9a4de0950094
Carlos Pineda <cara_p@unicah.edu>
2023-01-22 09:11:04 -0600

    Introduccion al proyecto

--- 05-Adivina-mi-numero/script.js
+++ 05-Adivina-mi-numero/script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+// SE utiliza para seleccionar un elemento del dom mediante los selectores de css
+document.querySelector('.message');
+
+// El textContent nos muestra el texto que contiene el elemento que seleccionamos
+console.log(document.querySelector('.message').textContent);
