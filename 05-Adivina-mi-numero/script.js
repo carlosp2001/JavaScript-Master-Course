b1546e49f07451ed68e8e6f7534e5c06bbcc69eb
Carlos Pineda <cara_p@unicah.edu>
2023-01-22 09:18:05 -0600

    Que es DOM y su manipulación

--- 05-Adivina-mi-numero/script.js
+++ 05-Adivina-mi-numero/script.js
@@ -5,3 +5,11 @@ document.querySelector('.message');
 
 // El textContent nos muestra el texto que contiene el elemento que seleccionamos
 console.log(document.querySelector('.message').textContent);
+
+/////////////////////////////////////////////
+// Que es el DOM y Manipulacion del DOM
+
+
+// El Document Object Model (DOM): Es una representacion estructurada de documentos HTML.
+// Permite a JavaScript acceder a elementos HTML y estilos para manipularlos.
+
