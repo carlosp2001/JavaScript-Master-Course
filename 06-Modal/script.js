c3274afa54b54090b11258ab3fe64782c4f3b453
Carlos Pineda <cara_p@unicah.edu>
2023-01-23 16:17:38 -0600

    Trabajando con clases desde JavaScript

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -4,8 +4,23 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModals = document.querySelectorAll('.show-modal');
-console.log(btnOpenModals);
+// console.log(btnOpenModals);
+
+const openModal = () => {
+	console.log('Button clicked');
+	// Removemos la clase hidden para que el modal sea visible
+	modal.classList.remove('hidden');
+	overlay.classList.remove('hidden');
+}
+
+const closeModal = () => {
+	modal.classList.add('hidden');
+	overlay.classList.add('hidden');
+}
 
 for (let i = 0; i < btnOpenModals.length; i++) {
-	console.log(btnOpenModals[i].textContent);
+	btnOpenModals[i].addEventListener('click', openModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal)
