236ae0da96d577bec131cee2b2eec436fc15315e
Carlos Pineda <cara_p@unicah.edu>
2023-01-24 21:33:08 -0600

    Cambiando el jugador actual

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -4,10 +4,13 @@
 
 // const score0 = document.querySelector('#score--0');
 // Otra manera de seleccionar elementos por ID
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.getElementById('current--0')
 const current1El = document.getElementById('current--1')
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -18,7 +21,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Lanzar dados funcionalidad
 btnRoll.addEventListener('click', () => {
@@ -33,8 +38,15 @@ btnRoll.addEventListener('click', () => {
 	if (dice !== 1) {
 		// Agregar el dado al puntaje actual
 		currentScore += dice;
-		current0El.textContent = currentScore; // Cambiar mas tarde
+		document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+
 	} else {
+		// Lo realizamos antes de hacer el switch
+		document.getElementById(`current--${activePlayer}`).textContent = 0;
 		// cambia al siguiente jugador
+		activePlayer = activePlayer === 0 ? 1 : 0;
+		currentScore = 0;
+		player0El.classList.toggle('player--active');
+		player1El.classList.toggle('player--active');
 	}
 });
