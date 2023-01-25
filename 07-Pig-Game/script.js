7896d407bc7134af050fb8750efaaf424d3302dc
Carlos Pineda <cara_p@unicah.edu>
2023-01-25 09:07:26 -0600

    Reiniciando el juego Proyecto #3 Terminado

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -16,15 +16,26 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Iniciando condiciones
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = () => {
+	// Iniciando condiciones
+	scores = [0, 0];
+	currentScore = 0;
+	activePlayer = 0;
+	playing = true;
+	score0El.textContent = 0;
+	score1El.textContent = 0;
+	current0El.textContent = 0;
+	current1El.textContent = 0;
+	diceEl.classList.add('hidden');
+	player0El.classList.remove('player--winner');
+	player1El.classList.remove('player--winner');
+	player0El.classList.add('player--active');
+	player1El.classList.remove('player--active');
+}
+
+init();
 
 const switchPlayer = () => {
 	// Lo realizamos antes de hacer el switch
@@ -65,7 +76,7 @@ btnHold.addEventListener('click', () => {
 		document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
 		// 2. Check if player's score is >= 100
-		if (scores[activePlayer] >= 10) {
+		if (scores[activePlayer] >= 100) {
 			// Finish the game
 			diceEl.classList.add('hidden');
 			playing = false;
@@ -78,4 +89,7 @@ btnHold.addEventListener('click', () => {
 		}
 	}
 });
+
+btnNew.addEventListener('click', init)
+
 
