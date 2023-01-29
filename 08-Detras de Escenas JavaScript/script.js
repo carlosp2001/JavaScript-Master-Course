d9b9aacf1b7b5bbf89ace5e0403258a5193fbb51
Carlos Pineda <cara_p@unicah.edu>
2023-01-29 08:44:26 -0600

    Elevacion y TDZ (Temporal Dead Zone)

--- 08-Detras de Escenas JavaScript/script.js
+++ 08-Detras de Escenas JavaScript/script.js
@@ -2,12 +2,13 @@
 
 function calcAge(birthYear) {
 	const age = 2037 - birthYear;
+
 	function printAge() {
 		let output = `${firstName}, you are ${age}, born in ${birthYear}`;
 		console.log(output);
 
 		if (birthYear >= 1981 && birthYear <= 1996) {
-			var	millenial = true;
+			var millenial = true;
 			// Creating NEW variable with same name as outer scope's variable
 			const firstName = 'Steven';
 
@@ -16,6 +17,7 @@ function calcAge(birthYear) {
 			// Las variables var tienen alcance de funcion por lo que se pueden usar fuera del bloque
 			const str = `Oh, and you're a millenial, ${firstName}`;
 			console.log(str);
+
 			function add(a, b) {
 				return a + b;
 			}
@@ -27,6 +29,7 @@ function calcAge(birthYear) {
 		console.log(output)
 
 	}
+
 	printAge();
 	return age;
 }
@@ -34,3 +37,52 @@ function calcAge(birthYear) {
 const firstName = 'Jonas';
 calcAge(1991);
 console.log(age);
+
+////////////////////////////////////
+// Elevacion (Hoisting y TDZ)
+
+// Elevacion con variables
+console.log(me);
+console.log(job);
+console.log(year);
+
+var me = 'Jonas';
+let job = 'Teacher';
+const year = 1991;
+
+// Elevacion con funciones
+console.log(addDecl(2, 3));
+console.log(addExpr(2, 3));
+console.log(addArrow(2, 3));
+
+// Al ser una funcion declarada en una variable entra automaticamente en la temporal dead zone (TDZ)
+// Al declararse con var las funciones se vuelven un valor undefined
+
+function addDecl(a, b) {
+	return a + b;
+}
+
+const addExpr = function (a, b) {
+	return a + b;
+}
+
+var addArrow = (a, b) => a + b;
+
+// Example
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+	console.log('All products deleted');
+}
+
+var z = 1;
+let y = 2;
+const z = 3;
+
+// Comparamos si la propiedad x declarada con var es un objeto que se encuentra en la ventana (window)
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
