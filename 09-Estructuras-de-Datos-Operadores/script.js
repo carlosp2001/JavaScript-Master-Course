e99fda1e4ee8dcc3664c9d7dbc89573d2988955f
Carlos Pineda <cara_p@unicah.edu>
2023-02-04 15:52:46 -0600

    Literales de objeto mejorado (Enhanced object literals)

--- 09-Estructuras-de-Datos-Operadores/script.js
+++ 09-Estructuras-de-Datos-Operadores/script.js
@@ -4,6 +4,25 @@
 const flights =
 	'_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+const weekdays1 = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+const openingHours = {
+	// ES6 objeto literal mejorado
+	[weekdays1[3]]: {
+		open: 12,
+			close: 22,
+	},
+	[weekdays1[4]]: {
+		open: 11,
+			close: 23,
+	},
+	[weekdays1[5]]: {
+		open: 0, // Open 24 hours
+			close: 24,
+	},
+};
+
+
 // Data needed for first part of the section
 const restaurant = {
 	name: 'Classico Italiano',
@@ -11,24 +30,12 @@ const restaurant = {
 	categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
 	starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
 	mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+	// ES6 objeto literal mejorado
+	openingHours,
 
-	order: function (starterIndex, mainIndex) {
+	// ES6 objeto literal mejorado
+	order(starterIndex, mainIndex) {
 		return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-	},
-
-	openingHours: {
-		thu: {
-			open: 12,
-			close: 22,
-		},
-		fri: {
-			open: 11,
-			close: 23,
-		},
-		sat: {
-			open: 0, // Open 24 hours
-			close: 24,
-		},
 	},
 
 	orderDelivery: function ({starterIndex = 1, mainIndex = 0, time = '20:00', address}) {
@@ -104,8 +111,8 @@ console.log(p, q, r);
 ////////////////////////////////////
 // Desestructuración de objetos
 
-const {name, openingHours, categories} = restaurant;
-console.log(name, openingHours, categories);
+const {name, openingHoursObj, categories} = restaurant;
+console.log(name, openingHoursObj, categories);
 
 // Asignarle nuevos nombres a las variables que obtenemos mediante la desestructuracion
 const {name: restaurantName, openingHours: hours, categories: tags} = restaurant;
@@ -413,4 +420,7 @@ for (const item of menu3) console.log(item);
 for (const [i, el] of menu3.entries()) console.log(`${i + 1}: ${el}`);
 
 // console.log([...menu3.entries()]);
+
+///////////////////////////////////////////////////////////
+// Literales de objeto mejorado (Enhanced object literals)
 
