696dfc72fd150234853c788e234c1b7d3b2522f1
Carlos Pineda <cara_p@unicah.edu>
2023-02-06 11:09:47 -0600

    Coding Challenge #2

--- 09-Estructuras-de-Datos-Operadores/script.js
+++ 09-Estructuras-de-Datos-Operadores/script.js
@@ -10,15 +10,15 @@ const openingHours = {
 	// ES6 objeto literal mejorado
 	[weekdays1[3]]: {
 		open: 12,
-			close: 22,
+		close: 22,
 	},
 	[weekdays1[4]]: {
 		open: 11,
-			close: 23,
+		close: 23,
 	},
 	[weekdays1[5]]: {
 		open: 0, // Open 24 hours
-			close: 24,
+		close: 24,
 	},
 };
 
@@ -446,8 +446,8 @@ for (const day of days) {
 }
 
 // Metodos en encadenamiento opcional
-console.log(restaurant.order?.(0,1) ?? 'Method does not exist');
-console.log(restaurant.orderRisotto?.(0,1) ?? 'Method does not exist');
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
 
 // Arrays
 const users = [
@@ -486,3 +486,57 @@ for (const [day, {open, close}] of entries) {
 	console.log(`On ${day} we open at ${open} and close at ${close}`);
 }
 
+///////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+
+// 1.
+for (const [i, player] of game.scored.entries()) {
+	console.log(`Goal ${i + 1}: ${player}`);
+}
+
+// 2.
+const odds = Object.values(game.odds);
+let average = 0;
+for (const odd of odds)
+	average += odd;
+
+average /= odds.length;
+console.log(average);
+
+// 3.
+for (const [team, odd] of Object.entries(game.odds)) {
+	const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`
+	console.log(`Odd of ${teamStr}... ${odd}`);
+}
+
+// Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+
+const scorers = {}
+// Bonus
+for (const player of game.scored) {
+	scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+console.log(scorers);
