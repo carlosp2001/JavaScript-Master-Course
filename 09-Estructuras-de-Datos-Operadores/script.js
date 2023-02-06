c30b74d907e0d52ed43b50d1ffc8f75e47980229
Carlos Pineda <cara_p@unicah.edu>
2023-02-06 10:57:58 -0600

    Objetos Iteradores: Llaves de objetos, valores y entradas

--- 09-Estructuras-de-Datos-Operadores/script.js
+++ 09-Estructuras-de-Datos-Operadores/script.js
@@ -460,3 +460,29 @@ const users = [
 console.log(users[0]?.name ?? 'User array empty');
 
 if (users.length > 0) console.log(users[0].name); else console.log('User array empty');
+
+//////////////////////////////////////////////////////////////
+// Objetos Iteradores: Llaves de objetos, valores y entradas
+
+// Recorriendo los nombres de las propiedades / keys
+// Propiedad Nombres / keys
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of Object.keys(openingHours)) openStr += `${day}, `;
+console.log(openStr)
+
+// Propiedad Valores
+const values = Object.values(openingHours);
+console.log(values);
+
+// Recorremos el objeto entero
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for (const [day, {open, close}] of entries) {
+	console.log(`On ${day} we open at ${open} and close at ${close}`);
+}
+
