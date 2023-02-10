65ebd0d4155888eff0b39a19580aacc3592e78ea
Carlos Pineda <cara_p@unicah.edu>
2023-02-10 13:23:27 -0600

    Mapas Iteracion

--- 09-Estructuras-de-Datos-Operadores/script.js
+++ 09-Estructuras-de-Datos-Operadores/script.js
@@ -633,3 +633,37 @@ console.log(rest.size);
 
 console.log(rest.get(arrRest));
 
+// Iteracion con mapas
+
+const question = new Map([
+    ['question', 'What is the best programming language in the world?'],
+    [1, 'C'],
+    [2, 'Java'],
+    [3, 'JavaScript'],
+    ['correct', 3],
+    [true, 'Correct'],
+    [false, 'Try again!']
+]);
+
+console.log(question);
+
+// Convertir objetos a mapas
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz App
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+const answer = 3;
+console.log(answer);
+console.log(question.get(question.get('correct') === answer));
+
+// Convertir mapa en arreglo
+console.log([...question]);
+console.log(question.entries());
+console.log(question.keys());
+console.log(question.values());
