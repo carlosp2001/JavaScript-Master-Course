d1e9575edd68c960f76cbdf4872844fd2365a458
Carlos Pineda <cara_p@unicah.edu>
2023-02-12 20:42:51 -0600

    Coding Challenge #4

--- 09-Estructuras-de-Datos-Operadores/script.js
+++ 09-Estructuras-de-Datos-Operadores/script.js
@@ -804,11 +804,111 @@ const checkBaggage = function (items) {
   const baggage = items.toLowerCase();
   if (baggage.includes("knife") || baggage.includes("gun")) {
     console.log("You're not allowed on board");
-  }else {
-    console.log('Welcome aboard!')
+  } else {
+    console.log("Welcome aboard!");
   }
 };
 
 checkBaggage("I have a laptop, some Food and a pocket Knife");
 checkBaggage("Socks and camera");
 checkBaggage("Got some snacks and a gun for protection");
+
+console.log("a+very+nice+string".split("+"));
+console.log("Jonas Schmedtmann".split(" "));
+
+// Split and join
+const [firstName, lastName] = "Jonas Schmedtmann".split(" ");
+const newName = ["Mr.", firstName, lastName.toUpperCase()].join(" ");
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(" ");
+  const namesUpper = [];
+  for (const n of names) {
+    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+  }
+  console.log(namesUpper.join(" "));
+};
+
+capitalizeName("jessica ann smith davis");
+capitalizeName("carlos pineda");
+
+// padding strings / rellenar cadenas
+const message1 = "Go to gate 23!";
+console.log(message1.padStart(25, "+").padEnd(30, "+"));
+console.log("Jonas".padStart(25, "+").padEnd(30, "+"));
+
+const maskCredit = function (number) {
+  const str = number + "";
+  const last = str.slice(-4);
+  return last.padStart(str.length, "*");
+};
+
+console.log(maskCredit(43333223544553));
+console.log(maskCredit("334333223332322"));
+
+// Metodo repetir
+const message2 = "Bad weather... All Departures Delayed...";
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${"🛩️".repeat(n)}`);
+};
+
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/*
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+document.body.append(document.createElement("textarea"));
+document.body.append(document.createElement("button"));
+
+document.querySelector("button").addEventListener("click", () => {
+  const text = document.querySelector("textarea").value;
+  const rows = text.split("\n");
+  console.log(rows);
+
+  for (const [i,row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split("_");
+    const output = `${first}${second.replace(second[0], second[0].toUpperCase())}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i+1)}`);
+  }
+});
+
+// underscoreCase -> underscoreCase
+// firstName
+// someVariable
+// calculateAge
+// delayedDeparture
