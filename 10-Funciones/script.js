4d5eabfd818c9abd23c3aab232c17adace4f086b
Carlos Pineda <cara_p@unicah.edu>
2023-02-16 21:31:49 -0600

    Como pasar argumentos funciona: Valores vs. Referencia

--- 10-Funciones/script.js
+++ 10-Funciones/script.js
@@ -25,3 +25,39 @@ createBooking('LH123', 5);
 
 // Saltarse un parametro
 createBooking('LH123', undefined, 1000);
+
+
+/////////////////////////////////////////////////////////////////////
+// Como el pasar argumentos funciona Valor vs Referencia
+
+const flight = 'LH234';
+const jonas = {
+	name: 'Jonas Schmedtmann',
+	passport: 243738219
+}
+
+const checkIn = function (flightNum, passenger) {
+	flightNum = 'LH999';
+	passenger.name = 'Mr. ' + passenger.name;
+
+	if (passenger.passport === 243738219) {
+		alert('Check in')
+	} else {
+		alert('Wrong password');
+	}
+}
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+// Es lo mismo que hacer...
+const flightNum = flight;
+const passenger = jonas;
+
+const newPassport = function (person) {
+	person.passport = Math.trunc(Math.random() * 10000000000);
+}
+
+newPassport(jonas);
+checkIn(flight, jonas);
