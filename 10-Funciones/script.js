a54e6a7d776f0d1a67e0fe8918fc783b783e2790
Carlos Pineda <cara_p@unicah.edu>
2023-02-17 13:31:04 -0600

    Métodos call y apply

--- 10-Funciones/script.js
+++ 10-Funciones/script.js
@@ -1,51 +1,54 @@
-'use strict';
+"use strict";
 
 //////////////////////////////////////////////////////
 // Parametros predeterminados / Default Parameters
 
 const bookings = [];
 
-const createBooking = function (flightNum, numPassengers = 1, price = 199 * numPassengers) {
-	// ES5 forma antigua
-	// numPassengers = numPassengers || 1;
-	// price = price || 199;
-	const booking = {
-		flightNum,
-		numPassengers,
-		price
-	}
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5 forma antigua
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
 
-	console.log(booking);
-	bookings.push(booking);
-}
+  console.log(booking);
+  bookings.push(booking);
+};
 
-createBooking('LH123');
-createBooking('LH123', 2, 800);
-createBooking('LH123', 5);
+createBooking("LH123");
+createBooking("LH123", 2, 800);
+createBooking("LH123", 5);
 
 // Saltarse un parametro
-createBooking('LH123', undefined, 1000);
-
+createBooking("LH123", undefined, 1000);
 
 /////////////////////////////////////////////////////////////////////
 // Como el pasar argumentos funciona Valor vs Referencia
 
-const flight = 'LH234';
+const flight = "LH234";
 const jonas = {
-	name: 'Jonas Schmedtmann',
-	passport: 243738219
-}
+  name: "Jonas Schmedtmann",
+  passport: 243738219,
+};
 
 const checkIn = function (flightNum, passenger) {
-	flightNum = 'LH999';
-	passenger.name = 'Mr. ' + passenger.name;
+  flightNum = "LH999";
+  passenger.name = "Mr. " + passenger.name;
 
-	if (passenger.passport === 243738219) {
-		alert('Check in')
-	} else {
-		alert('Wrong password');
-	}
-}
+  if (passenger.passport === 243738219) {
+    alert("Check in");
+  } else {
+    alert("Wrong password");
+  }
+};
 
 checkIn(flight, jonas);
 console.log(flight);
@@ -56,12 +59,11 @@ const flightNum = flight;
 const passenger = jonas;
 
 const newPassport = function (person) {
-	person.passport = Math.trunc(Math.random() * 10000000000);
-}
+  person.passport = Math.trunc(Math.random() * 10000000000);
+};
 
 newPassport(jonas);
 checkIn(flight, jonas);
-
 
 //////////////////////////////////////////////////////////////
 // Funciones de primera clase / Funciones de orden superior
@@ -78,49 +80,103 @@ función o ambas.
 // Funciones aceptando llamados de funciones
 
 const oneWord = function (str) {
-	return str.replace(/ /g, '').toLowerCase();
-}
+  return str.replace(/ /g, "").toLowerCase();
+};
 
 const upperFirstWord = function (str) {
-	const [first, ...others ] = str.split(' ');
-	return [first.toUpperCase(), ...others].join(' ');
-}
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
 
 // High Order Function / Funcion de orden superior
 const transformer = function (str, fn) {
-	console.log(`Original String: ${str}`)
-	console.log(`Transformed string: ${fn(str)}`);
-	console.log(`Transformed by: ${fn.name}`);
-}
+  console.log(`Original String: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
 
-transformer('JavaScript is the best', upperFirstWord);
-transformer('JavaScript is the best', oneWord);
+transformer("JavaScript is the best", upperFirstWord);
+transformer("JavaScript is the best", oneWord);
 
 // JS usa callbacks todo el tiempo
 const high5 = function () {
-	console.log('Hi!')
-}
+  console.log("Hi!");
+};
 
-document.body.addEventListener('click', high5);
+document.body.addEventListener("click", high5);
 
-['Jonas', 'Marta', 'Adam'].forEach(high5);
-
+["Jonas", "Marta", "Adam"].forEach(high5);
 
 ///////////////////////////////////////////////////////////////////
 // Funciones retornando funciones / functions returning functions
 
 const greet = function (greeting) {
-	return function (name) {
-		console.log(`${greeting} ${name}`)
-	}
-}
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
 
-const greeterHey = greet('Hey');
-greeterHey('Jonas');
-greeterHey('Steven');
+const greeterHey = greet("Hey");
+greeterHey("Jonas");
+greeterHey("Steven");
 
-greet('Hello')('Jonas');
+greet("Hello")("Jonas");
 
 // Challenge: Reescribir la función en arrow function
-const greetArr = greeting => name => console.log(`${greeting} ${name}`);
-greetArr('Hi')('Jonas');
+const greetArr = (greeting) => (name) => console.log(`${greeting} ${name}`);
+greetArr("Hi")("Jonas");
+
+/////////////////////////////////////////////
+// Metodos call y apply
+
+const lufthansa = {
+  airline: "Lufthansa",
+  iataCode: "LH",
+  bookings: [],
+  // book: function () {}
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, "Carlos Pineda");
+lufthansa.book(635, "John Smith");
+console.log(lufthansa)
+
+const eurowings = {
+  airline: "Eurowings",
+  iataCode: "EW",
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// No funciona porque estamos haciendo una llamado a la funcion normal, la palabra this en este contexto toma el valor
+// de undefined
+// book(23, "Sarah Williams");
+
+// Metodo call
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: []
+}
+
+book.call(swiss, 583, 'Mary Cooper');
+console.log(swiss);
+
+// Metodo apply
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
