b47bc5f9c58261143a8f6e708a01fdf415acb565
Carlos Pineda <cara_p@unicah.edu>
2023-02-21 13:20:08 -0600

    El nuevo metodo at

--- 11-Arreglos(Arrays)-Bankist/script.js
+++ 11-Arreglos(Arrays)-Bankist/script.js
@@ -115,3 +115,17 @@ console.log([...arr, ...arr2]);
 
 // JOIN //
 console.log(letters.join(' - '));
+
+////////////////////////////////////////////////
+// El nuevo método at
+
+const arr1 = [23, 11, 64];
+console.log(arr1[0]);
+console.log(arr.at(0));
+
+// Formas de conseguir el último elemento del array
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1));
+console.log(arr.at(-1));
+
+console.log('jonas'.at(-1));
