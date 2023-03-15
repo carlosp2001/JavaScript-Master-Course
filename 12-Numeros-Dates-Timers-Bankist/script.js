7f6c62060ec5e2f588a233e1d10360c514d490ae
Carlos Pineda <cara_p@unicah.edu>
2023-03-14 21:51:09 -0600

    Operaciones con fechas / modificamos las fechas de las transacciones

--- 12-Numeros-Dates-Timers-Bankist/script.js
+++ 12-Numeros-Dates-Timers-Bankist/script.js
@@ -22,8 +22,8 @@ const account1 = {
     "2020-04-01T10:17:24.185Z",
     "2020-05-08T14:11:59.604Z",
     "2020-05-27T17:01:17.194Z",
-    "2020-07-11T23:36:17.929Z",
-    "2020-07-12T10:51:36.790Z",
+    "2023-03-01T23:36:17.929Z",
+    "2023-03-15T10:51:36.790Z",
   ],
   currency: "EUR",
   locale: "pt-PT", // de-DE
@@ -81,6 +81,23 @@ const inputClosePin = document.querySelector(".form__input--pin");
 /////////////////////////////////////////////////
 // Functions
 
+// Funcion que convierte las diferencias de fechas en dias
+const formatMovementDate = (date) => {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+  const daysPassed = calcDaysPassed(new Date(), date);
+  console.log(daysPassed);
+  if (daysPassed === 0) return "Today";
+  if (daysPassed === 1) return "Yesterday";
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  const day = `${date.getDate()}`.padStart(2, "0");
+  const month = `${date.getMonth() + 1}`.padStart(2, "0");
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+
+  // const days1 = calcDaysPassedF(new Date(2037, 3, 14), new Date(2037, 3, 24));
+};
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = "";
 
@@ -92,10 +109,7 @@ const displayMovements = function (acc, sort = false) {
     const type = mov > 0 ? "deposit" : "withdrawal";
 
     const date = new Date(acc.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, "0");
-    const month = `${date.getMonth() + 1}`.padStart(2, "0");
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovementDate(date);
 
     const html = `
       <div class="movements__row">
@@ -515,3 +529,14 @@ console.log(new Date(2142278580000)); // Revierte la marca de tiempo y la convie
 console.log(Date.now());
 
 future.setFullYear(2040); // Cambiar el año a una fecha
+
+////////////////////////////////////////////////////////
+// Operaciones con fechas
+
+console.log(+future); // Nos devuelve el timestamp en milisegundos
+
+const calcDaysPassedF = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+const days1 = calcDaysPassedF(new Date(2037, 3, 14), new Date(2037, 3, 24));
+console.log(days1);
