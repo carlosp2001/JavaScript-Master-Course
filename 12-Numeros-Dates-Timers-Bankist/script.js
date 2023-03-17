afb5df715b514cc9b7f245bcd3e170802b46e12b
Carlos Pineda <cara_p@unicah.edu>
2023-03-17 15:03:42 -0600

    Timers: setTimeOut y setInterval

--- 12-Numeros-Dates-Timers-Bankist/script.js
+++ 12-Numeros-Dates-Timers-Bankist/script.js
@@ -292,14 +292,16 @@ btnLoan.addEventListener("click", function (e) {
     amount > 0 &&
     currentAccount.movements.some((mov) => mov >= amount * 0.1)
   ) {
-    // Add movement
-    currentAccount.movements.push(amount);
+    setTimeout(() => {
+      // Add movement
+      currentAccount.movements.push(amount);
 
-    // Add transfer date
-    currentAccount.movementsDates.push(new Date().toISOString());
+      // Add transfer date
+      currentAccount.movementsDates.push(new Date().toISOString());
 
-    // Update UI
-    updateUI(currentAccount);
+      // Update UI
+      updateUI(currentAccount);
+    }, 2500);
   }
   inputLoanAmount.value = "";
 });
@@ -595,3 +597,23 @@ console.log(
   navigator.language,
   new Intl.NumberFormat(navigator.language, options).format(numIntl)
 );
+
+//////////////////////////////////////////////
+// Timers: setTimeout y setInterval
+
+// setTimeOut
+const ingredients = ["olives", "spinach"];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2}!`),
+  3000,
+  ...ingredients
+);
+console.log("Waiting...");
+
+if (ingredients.includes("spinach")) clearTimeout(pizzaTimer); // Esto cancela el timer
+
+// setInterval
+setInterval(function () {
+  const now = new Date();
+  console.log(now);
+}, 3000)
