759961041d87232c78e0164ca84c03c664a98fe4
Carlos Pineda <cara_p@unicah.edu>
2023-03-24 15:16:13 -0600

    Atravesar el DOM (DOM Traversing)

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -320,3 +320,35 @@ document.querySelector('.nav').addEventListener('click', function (e) {
 
 /////////////////////////////////////////////////////////////////
 // Delegacion de Evento: Implementando navegacion de la pagina
+
+
+/////////////////////////////////////////////////////////////////
+// Atravesar el DOM (DOM Traversing)
+
+const h1 = document.querySelector('h1');
+
+// Going downwards (bajando): child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes); // Nos devuelve la lista de nodos contenidos
+console.log(h1.children); // Nos devuelve una coleccion HTML
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// Going upwards (subiendo): padres
+console.log(h1.parentNode); // Nos devuelve los elementos padres
+console.log(h1.parentElement);
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// Going sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling );
+
+console.log(h1.parentElement.children); // Metodo para recuperar todos los elementos hermanos
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+})
