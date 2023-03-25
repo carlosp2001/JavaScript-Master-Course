455e081ee0762b7f911528c60a4d05b44fc73866
Carlos Pineda <cara_p@unicah.edu>
2023-03-25 10:01:21 -0600

    Implementando una navegación sticky: El evento scroll

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -138,6 +138,16 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 nav.addEventListener('mouseout', handleHover.bind(1));
 
+// Navegacion sticky
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords);
+
+window.addEventListener('scroll', function (e) {
+  console.log(window.scrollY);
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+})
+
 /*
 ///////////////////////////////////////////////////////
 // Seleccionando, creando y eliminando elementos
@@ -401,3 +411,8 @@ console.log(h1.parentElement.children); // Metodo para recuperar todos los eleme
 
 ////////////////////////////////////////////////////////
 // Construyendo un componente con pestañas
+
+////////////////////////////////////////////////////////
+// Implementando una navegación sticky: El evento scroll
+
+
