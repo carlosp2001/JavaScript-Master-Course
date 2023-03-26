9979b56bae629c67eeb2830c58c33702886208be
Carlos Pineda <cara_p@unicah.edu>
2023-03-25 22:03:33 -0600

    Carga perezosa de imagenes

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -133,7 +133,7 @@ const handleHover = function (e, opacity) {
     });
     logo.style.opacity = opacity;
   }
-}
+};
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 nav.addEventListener('mouseout', handleHover.bind(1));
@@ -154,16 +154,18 @@ window.addEventListener('scroll', function (e) {
 const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
 
-const stickyNav = (entries) => {
+const stickyNav = entries => {
   const [entry] = entries;
   console.log(entry);
-  entry.isIntersecting ? nav.classList.remove('sticky') : nav.classList.add('sticky');
-}
+  entry.isIntersecting
+    ? nav.classList.remove('sticky')
+    : nav.classList.add('sticky');
+};
 
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
-  rootMargin: `-${navHeight}px` // Nos ayuda a especificar que margen queremos tener para que detecte la funcion
+  rootMargin: `-${navHeight}px`, // Nos ayuda a especificar que margen queremos tener para que detecte la funcion
 });
 headerObserver.observe(header);
 
@@ -176,19 +178,45 @@ const revealSection = function (entries, observer) {
   if (!entry.isIntersecting) return;
   entry.target.classList.remove('section--hidden');
   observer.unobserve(entry.target);
-}
+};
 
 const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
-  threshold: 0.15
+  threshold: 0.15,
 });
 
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
-  section.classList.add('section--hidden')
-})
+  section.classList.add('section--hidden');
+});
 
+// Lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+console.log(imgTargets);
 
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px'
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
 
 /*
 ///////////////////////////////////////////////////////
@@ -466,15 +494,15 @@ console.log(h1.parentElement.children); // Metodo para recuperar todos los eleme
 const obsCallback = function (entries, observer) {
   entries.forEach(entry => {
     console.log(entry);
-  })
+  });
 };
 
 const obsOptions = {
   root: null,
-  threshold: [0, 0.2] // Es el porcentaje de intersección en el que se llamara a la devolucion de llamada del
+  threshold: [0, 0.2], // Es el porcentaje de intersección en el que se llamara a la devolucion de llamada del
   // observador, 0 significa que se activara cada vez que el elemento este completamente fuera de la vista, 1 significa
   // que el elemento estará al 100% en la pantalla, en este caso no pasaria con la seccion 1
-}
+};
 
 const observer = new IntersectionObserver(obsCallback, obsOptions);
 observer.observe(section1);
