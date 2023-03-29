67eafe71a996d94ef9fdba2777768a8f8edefe14
Carlos Pineda <cara_p@unicah.edu>
2023-03-28 19:21:48 -0600

    Prototipos (Prototypes)

--- 14-Programación Orientada a Objetos (OOP) con JavaScript/script.js
+++ 14-Programación Orientada a Objetos (OOP) con JavaScript/script.js
@@ -1,22 +1,22 @@
-'use strict';
+"use strict";
 
 ////////////////////////////////////////////////
 // Funciones constructoras y el operador new
 
 // Ejemplo de funcion constructora
 const Person = function (firstName, birthYear) {
-	// Propiedades de la instancia
-	this.firstName = firstName;
-	this.birthYear = birthYear;
+  // Propiedades de la instancia
+  this.firstName = firstName;
+  this.birthYear = birthYear;
 
-	// Never create a method inside constructor function
-	// this.calcAge = function () {
-	// 	console.log(2037 - this.birthYear);
-	// };
-	console.log(this);
-}
+  // Never create a method inside constructor function
+  // this.calcAge = function () {
+  // 	console.log(2037 - this.birthYear);
+  // };
+  console.log(this);
+};
 
-const jonas = new Person('Jonas', 1991);
+const jonas = new Person("Jonas", 1991);
 console.log(jonas.firstName);
 
 // 1. New {} (Empty Object) is created
@@ -24,12 +24,37 @@ console.log(jonas.firstName);
 // 3. {} linked to prototype
 // 4. function automatically returns {}
 
-const matilda = new Person('Matilda', 2017);
-const jack = new Person('Jack', 1975);
+const matilda = new Person("Matilda", 2017);
+const jack = new Person("Jack", 1975);
 
 console.log(matilda, jack);
 
-const jay = 'Jay';
+const jay = "Jay";
 
 console.log(jonas instanceof Person);
 console.log(jay instanceof Person);
+
+////////////////////////////////////////////
+// Prototipos (Prototypes)
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+jack.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas, matilda);
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
