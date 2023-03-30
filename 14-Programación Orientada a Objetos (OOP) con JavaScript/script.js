46bd85ed75d57cce18211007870ce45c2e8bd992
Carlos Pineda <cara_p@unicah.edu>
2023-03-30 15:18:43 -0600

    Coding Challenge #2 y Object.create

--- 14-Programación Orientada a Objetos (OOP) con JavaScript/script.js
+++ 14-Programación Orientada a Objetos (OOP) con JavaScript/script.js
@@ -153,9 +153,9 @@ class PersonCl {
 
   // Set a property that already exists
   set fullName(name) {
-    console.log(name)
-    if (name.includes(' '))  this._fullName = name
-    else alert(`${name} is not a full name!`)
+    console.log(name);
+    if (name.includes(" ")) this._fullName = name;
+    else alert(`${name} is not a full name!`);
   }
 
   get fullName() {
@@ -164,7 +164,7 @@ class PersonCl {
 
   // Static method
   static hey() {
-    console.log('Hey there');
+    console.log("Hey there");
     console.log(this);
   }
 }
@@ -190,7 +190,7 @@ jessica.greet();
 ///////////////////////////////////////////////////////////
 // Setters y getters
 
-const walter = new PersonCl('Walter White', 1965)
+const walter = new PersonCl("Walter White", 1965);
 PersonCl.hey();
 
 const account = {
@@ -203,7 +203,6 @@ const account = {
   set latest(mov) {
     this.movements.push(mov);
   },
-
 };
 
 console.log(account.latest);
@@ -231,7 +230,79 @@ const Person1 = function (firstName, birthYear) {
 
 // No hay forma de que este método sea heredado, simplemente esta en el constructor
 Person1.hey = function () {
-  console.log('Hey there');
+  console.log("Hey there");
 };
 
 Person1.hey();
+
+////////////////////////////////////////////////////////
+// Object.create
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = "Steven";
+steven.birthYear = 2002;
+steven.calcAge();
+
+console.log(steven.__proto__ === PersonProto);
+
+const sarah = Object.create(PersonProto);
+sarah.init("Sarah", 1979);
+sarah.calcAge();
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/*
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+DATA CAR 1: 'Ford' going at 120 km/h
+
+GOOD LUCK 😀
+*/
+
+class CarCl {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate () {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  brake () {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const ford = new CarCl('Ford', 120);
+console.log(ford.speedUS);
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+ford.speedUS = 50;
+console.log(ford);
