2d30cf9bdaf89e9c665c49161e7a9fdff1e933b7
Carlos Pineda <cara_p@unicah.edu>
2023-03-31 11:06:33 -0600

    Herencia entre clases: Clases ES6

--- 14-Programación Orientada a Objetos (OOP) con JavaScript/script.js
+++ 14-Programación Orientada a Objetos (OOP) con JavaScript/script.js
@@ -401,3 +401,73 @@ console.log(tesla);
 tesla.brake();
 tesla.accelerate();
 console.log(tesla);
+
+///////////////////////////////////////////////////////
+// Herencia entre clases: Clases ES6
+
+class PersonC {
+  constructor(fullName, birthYear) {
+    // Propiedades
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Metodos de instancia
+  // Métodos se agregarán a la propiedad .prototype
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  // Set a property that already exists
+  set fullName(name) {
+    console.log(name);
+    if (name.includes(" ")) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  // Static method
+  static hey() {
+    console.log("Hey there");
+    console.log(this);
+  }
+}
+
+// Si no necesitas agregar una nueva propiedad en la clase hijo no es necesario crear un nuevo constructor simplemente
+// se utiliza la palabra extends
+class StudentC extends PersonC {
+  constructor(fullName, birthYear, course) {
+    super(fullName, birthYear); // Es la función constructora de la clase padre, necesita pasar siempre primero
+    this.course = course;
+  }
+
+  introduce() {
+    console.log(`My name is ${this.fullName} and I study ${this.course}`);
+  }
+
+  // Sobreescribir metodo
+  calcAge() {
+    console.log(
+      `I'm ${
+        2037 - this.birthYear
+      } years old, but as a student I feel more like ${
+        2037 - this.birthYear + 10
+      } `
+    );
+  }
+}
+
+const martha = new StudentC("Martha Jones", 2012, "Computer Science");
+martha.introduce();
+martha.calcAge();
