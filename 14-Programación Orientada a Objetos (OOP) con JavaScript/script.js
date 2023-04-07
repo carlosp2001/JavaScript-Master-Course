4022a0e049fbc2792e866232c9f0cf4066e4c15f
Carlos Pineda <cara_p@unicah.edu>
2023-04-07 10:15:24 -0600

    Encapsulación: Campos de clases privadas y métodos

--- 14-Programación Orientada a Objetos (OOP) con JavaScript/script.js
+++ 14-Programación Orientada a Objetos (OOP) con JavaScript/script.js
@@ -508,27 +508,32 @@ class Account {
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currrency = currency;
-    this.pin = pin;
-    this.movements = [];
+    // protected property
+    this._pin = pin;
+    this._movements = [];
     this.locale = navigator.language;
     console.log(`Thanks for openning an account, ${owner}`);
   }
 
   // Public interface of our objects
+  getMovements() {
+    return this._movements;
+  }
+
   deposit(val) {
-    this.movements.push(val)
+    this._movements.push(val)
   }
 
   withdraw(val) {
     this.deposit(-val)
   }
 
-  approveLoan(val) {
+  _approveLoan(val) {
     return true;
   }
 
   requestLoan(val) {
-    if (this.approveLoan(val)) {
+    if (this._approveLoan(val)) {
       this.deposit(val);
       console.log(`Loan approved`);
     }
@@ -537,13 +542,17 @@ class Account {
 
 const acc1 = new Account('Jonas', 'EUR', 1111, []);
 
-// acc1.movements.push(250);
-// acc1.movements.push(-140);
+acc1._movements.push(250);
+acc1._movements.push(-140);
 acc1.deposit(250);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
-acc1.approveLoan(1000);
+acc1._approveLoan(1000);
+console.log(acc1.getMovements())
 
 console.log(acc1);
-console.log(acc1.pin);
+console.log(acc1._pin);
+
+///////////////////////////////////////////////////////////
+// Encapsulación: Propiedades protegidas y métodos
 
