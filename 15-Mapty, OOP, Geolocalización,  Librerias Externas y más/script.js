e5adf5373f22f97acb3155aa1dd16a70fdbc462f
Carlos Pineda <cara_p@unicah.edu>
2023-04-13 20:47:03 -0600

    Mover al marcador en un click

--- 15-Mapty, OOP, Geolocalización,  Librerias Externas y más/script.js
+++ 15-Mapty, OOP, Geolocalización,  Librerias Externas y más/script.js
@@ -3,6 +3,7 @@
 class Workout {
     date = new Date();
     id = (Date.now() + '').slice(-10);
+    clicks = 0;
 
     constructor(coords, distance, duration) {
         this.coords = coords; // [lat, lng]
@@ -17,6 +18,10 @@ class Workout {
         this.description = `${this.type[0].toUpperCase()}${this.type.slice(
             1
         )} on ${months[this.date.getMonth()]} ${this.date.getDate()}`;
+    }
+
+    click() {
+        this.clicks++;
     }
 }
 
@@ -69,6 +74,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 // APPLICATION ARCHITECTURE
 class App {
     #map;
+    #mapZoomLevel = 13;
     #mapEvent;
     #workouts = [];
 
@@ -76,6 +82,10 @@ class App {
         this._getPosition();
         form.addEventListener('submit', this._newWorkout.bind(this));
         inputType.addEventListener('change', this._toggleElevationField);
+        containerWorkouts.addEventListener(
+            'click',
+            this._moveToPopup.bind(this)
+        );
     }
 
     _getPosition() {
@@ -93,7 +103,7 @@ class App {
         console.log(latitude, longitude);
 
         const coords = [latitude, longitude];
-        this.#map = L.map('map').setView(coords, 13); // El segundo parametro hace referencia a la cantidad de zoom
+        this.#map = L.map('map').setView(coords, this.#mapZoomLevel); // El segundo parametro hace referencia a la cantidad de zoom
         // del mapa
         console.log(this.#map);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -255,6 +265,26 @@ class App {
         }
 
         form.insertAdjacentHTML('afterend', html);
+    }
+
+    _moveToPopup(e) {
+        const workoutEl = e.target.closest('.workout');
+        console.log(workoutEl);
+        if (!workoutEl) return;
+        const workout = this.#workouts.find(
+            work => work.id === workoutEl.dataset.id
+        );
+        console.log(workoutEl);
+
+        this.#map.setView(workout.coords, this.#mapZoomLevel, {
+            animate: true,
+            pan: {
+                duration: 1,
+            },
+        });
+
+        // using the public interfaces
+        workout.click();
     }
 }
 
