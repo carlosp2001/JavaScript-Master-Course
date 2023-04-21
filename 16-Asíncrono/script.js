06568c7848f40efb96cd46bd4f48e749fa387f22
Carlos Pineda <cara_p@unicah.edu>
2023-04-21 11:37:08 -0600

    Consumiendo promesas con async/await

--- 16-Asíncrono/script.js
+++ 16-Asíncrono/script.js
@@ -5,25 +5,25 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-// const renderCountry = function (data, className = '') {
-//     const html = `<article class='country ${className}'>
-//           <img class='country__img' src='${data.flag}' />
-//           <div class='country__data'>
-//             <h3 class='country__name'>${data.name}</h3>
-//             <h4 class='country__region'>${data.region}</h4>
-//             <p class='country__row'><span>👫</span>${(
-//                 +data.population / 1000000
-//             ).toFixed(1)} people</p>
-//             <p class='country__row'><span>🗣️</span>${data.languages[0].name}</p>
-//             <p class='country__row'><span>💰</span>${
-//                 data.currencies[0].name
-//             }</p>
-//           </div>
-//         </article>`;
-//
-//     countriesContainer.insertAdjacentHTML('beforeend', html);
-//     countriesContainer.style.opacity = 1;
-// };
+const renderCountry = function (data, className = '') {
+    const html = `<article class='country ${className}'>
+          <img class='country__img' src='${data.flag}' />
+          <div class='country__data'>
+            <h3 class='country__name'>${data.name}</h3>
+            <h4 class='country__region'>${data.region}</h4>
+            <p class='country__row'><span>👫</span>${(
+                +data.population / 1000000
+            ).toFixed(1)} people</p>
+            <p class='country__row'><span>🗣️</span>${data.languages[0].name}</p>
+            <p class='country__row'><span>💰</span>${
+                data.currencies[0].name
+            }</p>
+          </div>
+        </article>`;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+};
 //
 // const getCountryAndNeighbour = function (country) {
 //     // AJAX Call country 1
@@ -411,3 +411,46 @@ createImage('img/img-1.jpg')
         currentImg.style.display = 'none';
     })
     .catch(err => console.error(err));
+
+////////////////////////////////////////////////////
+// Consumiendo promesas con async/await
+
+const getPosition = function () {
+    return new Promise(function (resolve, reject) {
+        // navigator.geolocation.getCurrentPosition(
+        //     position => resolve(position),
+        //     err => reject(err)
+        // );
+
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+};
+
+const whereAmI = async function () {
+    // Geolocation
+    const pos = await getPosition();
+    const { latitude: lat, longitude: lng } = pos.coords;
+
+    // Reverse geocoding
+    const resGeo = await fetch(
+        `https://geocode.xyz/${lat},${lng}?geoit=json&auth=418972686597070382439x92824`
+    );
+
+    const dataGeo = await resGeo.json();
+    console.log(dataGeo);
+
+    // Country data
+    // fetch(`https://restcountries.com/v2/name/${country}`).then(res =>
+    //     console.log(res)
+    // );
+
+    // Await nos ayuda a esperar al codigo hasta que la promesa se cumpla
+    const res = await fetch(`https://restcountries.com/v2/name/${dataGeo.prov}`);
+    // console.log(res);
+    const data = await res.json();
+    console.log(data);
+    renderCountry(data[0]);
+};
+
+whereAmI();
+console.log('FIRST');
