e3e71453ae02e64c4adbd4debfba17570023a4e9
Carlos Pineda <cara_p@unicah.edu>
2023-04-23 17:29:10 -0600

    Usando await en codigo de alto nivel

--- 17-Modulos, Herramientas JS Moderno/script.js
+++ 17-Modulos, Herramientas JS Moderno/script.js
@@ -8,17 +8,18 @@
 // console.log(price, tq);
 // // console.log(shippingCost);
 
-
-
 // console.log('Importing Module')
 // import * as ShoppingCart from './shoppingCart.js'
 //
 // ShoppingCart.addToCart('bread', 5);
 // console.log(ShoppingCart.totalPrice);
 
-
-
-import add, { addToCart, totalPrice as price, tq, cart } from './shoppingCart.js';
+import add, {
+    addToCart,
+    totalPrice as price,
+    tq,
+    cart,
+} from './shoppingCart.js';
 add('pizza', 6);
 add('bread', 5);
 add('apples', 4);
@@ -26,3 +27,29 @@ add('apples', 4);
 console.log(price);
 console.log(cart); // En este caso se demuestra la conexion que tenemos con el
 // modulo de exportación
+
+///////////////////////////////////////////////////////////////////////
+// Usando await en codigo de alto nivel
+
+// De esta forma ya no se necesita el uso de una funcion async
+console.log('Start fetching');
+const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+const data = await res.json();
+console.log(data);
+console.log('Something');
+
+const getLastPost = async function () {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await res.json();
+    console.log(data);
+    return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+//Not very clean
+lastPost.then(last => console.log(last))
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
